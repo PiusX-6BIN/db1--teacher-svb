c40d4c4c9146e4e37da499d72fbe4b615ac120e8
Sam Van Battel <teacher.svb@gmail.com>
2020-11-23 09:39:42 +0100

    Update data.php

--- opdracht5/data.php
+++ opdracht5/data.php
@@ -16,7 +16,9 @@ if ($conn === false) {
 $recept = array();
 
 $query = "
-    SELECT * FROM Recepten;
+    SELECT * 
+    FROM Recepten
+    WHERE Recepten.naam = 'tripel';
 ";
 
 $result = $conn->query($query);
@@ -29,6 +31,30 @@ for ($i = 0; $i < $numRows; ++$i) {
     $recept["beginSG"] = $row["beginSG"];
     $recept["eindvolume"] = $row["eindvolume"];
     $recept["gist"] = $row["gist"];
+}
+
+$query = "
+    SELECT ReceptHop.hopsoort, ReceptHop.hoeveelheid, ReceptHop.kooktijd, Hop.alpha
+    FROM Recepten
+    INNER JOIN ReceptHop ON ReceptHop.recept = Recepten.naam
+    INNER JOIN Hop		 ON ReceptHop.hopsoort = Hop.naam
+    WHERE Recepten.naam = 'tripel';
+";
+
+$result = $conn->query($query);
+
+$recept["hop"] = array();
+$numRows = mysqli_num_rows($result);
+for ($i = 0; $i < $numRows; ++$i) {
+    $row = $result->fetch_assoc();
+    
+    $hop = array();
+    $hop["hoeveelheid"] = $row["hoeveelheid"];
+    $hop["soort"] = $row["hopsoort"];
+    $hop["alpha"] = $row["alpha"];
+    $hop["kooktijd"] = $row["kooktijd"];
+
+    $recept["hop"][] = $hop;
 }
 
 $conn->close();
