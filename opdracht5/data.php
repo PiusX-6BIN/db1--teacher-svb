1a74a4d035c5d247a8f88df42e7a340a1f5ea0ce
Sam Van Battel <teacher.svb@gmail.com>
2020-11-23 09:28:56 +0100

    +

--- opdracht5/data.php
+++ opdracht5/data.php
@@ -13,6 +13,8 @@ if ($conn === false) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+$recept = array();
+
 $query = "
     SELECT * FROM Recepten;
 ";
@@ -20,14 +22,18 @@ $query = "
 $result = $conn->query($query);
 
 $numRows = mysqli_num_rows($result);
-
 for ($i = 0; $i < $numRows; ++$i) {
     $row = $result->fetch_assoc();
+
+    $recept["titel"] = $row["naam"];
+    $recept["beginSG"] = $row["beginSG"];
+    $recept["eindvolume"] = $row["eindvolume"];
+    $recept["gist"] = $row["gist"];
 }
 
 $conn->close();
 
-// echo json_encode();
+echo json_encode($recept);
 
 
 ?>
